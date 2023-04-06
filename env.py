335a4ba775c8d356fd0ea51a8b6fe1d3fc9e8d7f
Alena18 <coffeebeanstudio1809@gmail.com>
2023-04-06 22:00:26 +0000

    Create app files, database, env.py file, Procfile, deployment commit

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://uhjpvjyd:YJISJeCPKt9aIBJnSCHj4NJ68aJvVCzv@trumpet.db.elephantsql.com/uhjpvjyd"
+os.environ["SECRET_KEY"] = "7.sisterblog@ydacha"
+os.environ["CLOUDINARY_URL"] = "cloudinary://114968985793866:odkxsr-BrXh-p_HDmPGh23p5dJU@dmie5ezad"
---
+++ victoriabakeryblog/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class VictoriabakeryblogConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'victoriabakeryblog'
