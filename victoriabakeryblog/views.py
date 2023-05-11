3e98cdbf145b02f9fcb4ad26c6d0d3bc2fba3205
Alena18 <>
2023-05-11 22:29:37 +0000

    Debug templates

--- victoriabakeryblog/views.py
+++ victoriabakeryblog/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.views import generic
-from .model import Post
+from .models import RecipePost
 
 class PostList(generic.ListView):
-    model = Post
-    queryset = Post.object.filter(status=1).order_by('-created_on')
+    model = RecipePost
+    queryset = RecipePost.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 4
 
